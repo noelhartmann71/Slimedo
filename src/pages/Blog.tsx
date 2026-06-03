bae88329e5ae82fd22271816c95f1250093fdb79
noelhartmann71 <noel.hartmann71@gmail.com>
2026-06-03 10:21:12 +0200

    add slimedoticker

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -3,11 +3,13 @@ import BlogCard from "@/components/Blog/BlogCard";
 import CTABanner from "@/components/CTABanner/CTABanner";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import SlimedoTicker from "@/components/SlimedoTicker/SlimedoTicker.tsx";
 
 const Blog = () => {
   return (
     <div>
       {/* This is the Blog page */}
+      <SlimedoTicker/>
       <Navbar />
       <BlogBanner />
       <BlogCard />
--- src/pages/BlogDetails.tsx
+++ src/pages/BlogDetails.tsx
@@ -2,10 +2,12 @@ import BlogDetail from "@/components/BlogDetails/BlogDetail";
 import CTABanner from "@/components/CTABanner/CTABanner";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import SlimedoTicker from "@/components/SlimedoTicker/SlimedoTicker.tsx";
 
 const BlogDetails = () => {
   return (
     <div>
+      <SlimedoTicker/>
       <Navbar />
       <BlogDetail />
       <CTABanner />
